a2691aa67f9683aa2f511bc4cfcbfc8aa33d6422
Airmole <745362916@qq.com>
2026-08-30 16:15:55 +0800

    追加补充被AI删除的数据格式实例gist文档链接

--- src/Misc.php
+++ src/Misc.php
@@ -9,6 +9,7 @@ class Misc extends Base
     /**
      * 获取数据字典
      *
+     * @see https://gist.github.com/Airmole/147443e2f1ed0222769f033794e6fb09
      * @return array
      * @throws Exception
      */
